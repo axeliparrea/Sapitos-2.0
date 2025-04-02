60b6ebb5fa948ce8441e917e092acc17ec686b0e
Adrian Guereque <aguereque96@gmail.com>
2025-04-01 22:32:06 -0600

    Home and test page for dueno

---
+++ frontend/src/pages/dueno/InvoiceListPage.jsx
@@ -0,0 +1,17 @@
+import MasterLayout from "../../components/masterLayout";
+import InvoiceListLayer from "../../components/InvoiceListLayer";
+
+const InvoiceListPage = () => {
+  return (
+    <>
+      {/* MasterLayout */}
+      <MasterLayout role="dueno">
+
+        {/* InvoiceListLayer */}
+        <InvoiceListLayer />
+      </MasterLayout>
+    </>
+  );
+};
+
+export default InvoiceListPage;
