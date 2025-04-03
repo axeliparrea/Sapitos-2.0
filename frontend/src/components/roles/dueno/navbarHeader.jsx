17b1ccd6531324d2f1b5dd0958da99ddb07887f8
Adrian Guereque <aguereque96@gmail.com>
2025-04-02 18:35:01 -0600

    Made userMenu component for ease of use

--- frontend/src/components/roles/dueno/navbarHeader.jsx
+++ frontend/src/components/roles/dueno/navbarHeader.jsx
@@ -1,5 +1,5 @@
 import { Icon } from "@iconify/react";
-import { Link } from "react-router-dom";
+import UserMenu from "../../general/userMenu";
 
 const NavbarHeader = ({ sidebarActive, sidebarControl, mobileMenuControl }) => {
   return (
@@ -24,39 +24,12 @@ const NavbarHeader = ({ sidebarActive, sidebarControl, mobileMenuControl }) => {
           </div>
         </div>
 
-        {/* Menú del usuario */}
-        <div className="col-auto">
-          <div className="d-flex flex-wrap align-items-center gap-3">
-            <div className="dropdown">
-              <button className="d-flex justify-content-center align-items-center rounded-circle" type="button" data-bs-toggle="dropdown">
-                <img src="assets/images/user.png" alt="image_user" className="w-40-px h-40-px object-fit-cover rounded-circle" />
-              </button>
-              <div className="dropdown-menu to-top dropdown-menu-sm">
-                <div className="py-12 px-16 radius-8 bg-primary-50 mb-16 d-flex align-items-center justify-content-between gap-2">
-                  <div>
-                    <h6 className="text-lg text-primary-light fw-semibold mb-2">Sheccid Leija</h6>
-                    <span className="text-secondary-light fw-medium text-sm">Admin</span>
-                  </div>
-                  <button type="button" className="hover-text-danger">
-                    <Icon icon="radix-icons:cross-1" className="icon text-xl" />
-                  </button>
-                </div>
-                <ul className="to-top-list">
-                  <li>
-                    <Link className="dropdown-item text-black px-0 py-8 hover-bg-transparent hover-text-primary d-flex align-items-center gap-3" to="#">
-                      <Icon icon="solar:user-linear" className="icon text-xl" /> Mi Perfil
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className="dropdown-item text-black px-0 py-8 hover-bg-transparent hover-text-danger d-flex align-items-center gap-3" to="/">
-                      <Icon icon="lucide:power" className="icon text-xl" /> Cerrar Sesión
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
+        <UserMenu
+          name="Sheccid Leija"
+          role="Admin"
+          profileImage="assets/images/user.png"
+          onClose={() => console.log("Cerrar menú")}
+        />
       </div>
     </div>
   );
