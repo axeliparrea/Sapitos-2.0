150c45f1de55c0e8a7817ae02e950115bf2f3ba8
Adrian Guereque <aguereque96@gmail.com>
2025-04-07 19:04:15 -0600

    Added pages for cliente role

---
+++ frontend/src/pages/cliente/Home.jsx
@@ -0,0 +1,14 @@
+import MasterLayout from "../../components/masterLayout";
+
+const HomeCliente = () => {
+  return (
+    <>
+      {/* MasterLayout */}
+      <MasterLayout role="cliente">
+        <h1>Ordenes</h1>
+      </MasterLayout>
+    </>
+  );
+};
+
+export default HomeCliente;
---
+++ frontend/src/pages/cliente/Inventario.jsx
@@ -0,0 +1,14 @@
+import MasterLayout from "../../components/masterLayout";
+
+const Inventario = () => {
+  return (
+    <>
+      {/* MasterLayout */}
+      <MasterLayout role="cliente">
+        <h1>Inventario</h1>
+      </MasterLayout>
+    </>
+  );
+};
+
+export default Inventario;
