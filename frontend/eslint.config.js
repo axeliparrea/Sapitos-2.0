1973f3d7269c5ad20265dab2217f62ae0e37d67a
axeliparrea <141780766+axeliparrea@users.noreply.github.com>
2025-05-15 00:00:15 -0600

    global eslint

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,6 +3,7 @@ import js from '@eslint/js';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import jsxA11y from 'eslint-plugin-jsx-a11y';
+import globals from 'globals';
 
 export default [
   js.configs.recommended,
@@ -12,9 +13,9 @@ export default [
       sourceType: 'module',
       ecmaVersion: 2021,
       globals: {
-        window: true,
-        document: true,
-        console: true,
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2021,
       },
       parserOptions: {
         ecmaFeatures: {
@@ -28,10 +29,11 @@ export default [
       'jsx-a11y': jsxA11y,
     },
     rules: {
-      'react/react-in-jsx-scope': 'warn',
+      'react/react-in-jsx-scope': 'off',
       'react/prop-types': 'warn',
       'react-hooks/rules-of-hooks': 'warn',
       'react-hooks/exhaustive-deps': 'warn',
+      'no-undef': 'off',
       // 'react/react-in-jsx-scope': 'off',
       // 'react/prop-types': 'off',
       // 'react-hooks/rules-of-hooks': 'error',
