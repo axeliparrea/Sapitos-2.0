37f64e2f3b6531c5ddebf1d0e68d6280778c7104
axeliparrea <141780766+axeliparrea@users.noreply.github.com>
2025-05-15 00:03:15 -0600

    eliminacion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,5 @@
 import { useState, useEffect } from "react";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
-
-import SignInPage from "./pages/SignInPage";
-
-import HomeAdmin from "./pages/admin/Home";
-import InventarioAdmin from "./pages/admin/Inventario";
-
-import HomeDueno from "./pages/dueno/Home";
-import InventarioDueno from "./pages/dueno/Inventario";
-import OrdenesProveedoresDueno from "./pages/dueno/OrdenesProveedores";
-import OrdenesClientesDueno from "./pages/dueno/OrdenesClientes";
-import RecomendacionesIADueno from "./pages/dueno/RecomendacionesIA";
-
-import HomeCliente from "./pages/cliente/Home";
-import InventarioCliente from "./pages/cliente/Inventario"
-
-import HomeProveedor from "./pages/proveedor/Home";
-import OrdenesProveedor from "./pages/proveedor/Ordenes"
-
-import Pedidos from "./pages/admin/Pedidos"
-
-// Para usuarios admin 
-import UsuariosShec from "./pages/admin/UsuariosShec";
-import AddUserLayer from "./components/AddUserLayer";
-import InvoiceAddLayer from "./components/InvoiceAddLayer";
 
 const App = () => {
   const [role, setRole] = useState(null); // Initially null
